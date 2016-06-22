95bc09cddf8ca67295acd85eb9c04eee4701cfe3
Howard Smith <howardsmithnz@gmail.com>
2016-06-22 18:13:36 +1200

    add mongodb and POST route

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 var port = process.env.PORT || 8080 // set our port
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o') // connect to our database
+mongoose.connect('mongodb://localhost/ExpressREST') // connect to our database
 
 var Bear = require('./app/models/bear')
 
@@ -37,6 +37,33 @@ router.get('/', function (req, res) {
 })
 
 // more routes for our API will happen here
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/bears')
+
+  // create a bear (accessed at POST http://localhost:8080/api/bears)
+  .post(function (req, res) {
+    var bear = new Bear() // create a new instance of the Bear model
+    bear.name = req.body.name; // set the bears name (comes from the request)
+
+    // save the bear and check for errors
+    bear.save(function (err) {
+      if (err)
+        res.send(err)
+
+      res.json({ message: 'Bear created!' })
+    })
+  })
+  // get all the bears (accessed at GET http://localhost:8080/api/bears)
+  .get(function (req, res) {
+    Bear.find(function (err, bears) {
+      if (err)
+        res.send(err)
+
+      res.json(bears)
+    })
+  })
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
